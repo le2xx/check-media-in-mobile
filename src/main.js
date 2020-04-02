28221d9ca4c273f86670a63e3c7e18748282fdd7
le2xx <le2x@rambler.ru>
2020-04-02 13:58:16 +0300

    Added objects window screen

--- src/main.js
+++ src/main.js
@@ -2,5 +2,30 @@ import './styles/base.styl';
 // import { testBlock } from './app/components/test-block/test-block';
 
 window.onload = () => {
-  // testBlock();
+  const body = document.querySelector('body');
+  const screenTemplate = body.querySelector('.screen-info');
+
+
+  const addElement = (key, value) => {
+    const node = document.createElement('span');
+    node.innerHTML = `<span class="screen-key">${key}: </span><span class="screen-value">${value}</span><br>`;
+    return node;
+  };
+
+  const addScreenOptions = () => {
+    for (const key in window.screen) {
+      if (key === 'orientation') {
+        screenTemplate.appendChild(addElement(key, window.screen[key].type));
+        return;
+      }
+      screenTemplate.appendChild(addElement(key, window.screen[key]));
+    }
+  };
+
+  addScreenOptions();
+
+  window.addEventListener('resize', () => {
+    screenTemplate.innerHTML = '';
+    addScreenOptions();
+  });
 };
